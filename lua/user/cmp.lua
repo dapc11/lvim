badf25c03704650158e9ec2763ccf8c81f451da0
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-10-06 20:33:37 +0200

    Add custom setup of cmp

---
+++ lua/user/cmp.lua
@@ -0,0 +1,150 @@
+local cmp_ok, cmp = pcall(require, "cmp")
+if not cmp_ok or cmp == nil then
+  cmp = {
+    mapping = function(...) end,
+    setup = { filetype = function(...) end, cmdline = function(...) end },
+    config = { sources = function(...) end },
+  }
+end
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "ﰠ",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "פּ",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local mapping = {
+  ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+  ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  ["<C-e>"] = cmp.mapping({
+    i = cmp.mapping.abort(),
+    c = cmp.mapping.close(),
+  }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+  ["<CR>"] = cmp.mapping({
+    i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+    c = function(fallback)
+      if cmp.visible() then
+        cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+      else
+        fallback()
+      end
+    end,
+  }),
+  ["<Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif has_words_before() then
+      fallback()
+    else
+      fallback()
+    end
+  end, { "i", "s", "c" }),
+  ["<S-Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    else
+      fallback()
+    end
+  end, { "i", "s", "c" }),
+  ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
+  ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
+}
+
+lvim.builtin.cmp.sources = {
+  {
+    name = "nvim_lsp",
+    entry_filter = function(entry, ctx)
+      return require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind()
+    end,
+  },
+  { name = "buffer", max_item_count = 5, keyword_length = 5 },
+  { name = "path", max_item_count = 5 },
+  { name = "luasnip", max_item_count = 3 },
+  { name = "nvim_lua" },
+  { name = "treesitter" },
+}
+lvim.builtin.cmp.experimental = {
+  ghost_text = false,
+  native_menu = false,
+  custom_menu = true,
+}
+local cmp_sources = {
+  nvim_lsp = "[Lsp]",
+  nvim_lua = "[Lua]",
+  luasnip = "[Snip]",
+  buffer = "[Buf]",
+  path = "[Path]",
+}
+lvim.builtin.cmp.formatting = {
+  fields = { "kind", "abbr", "menu" },
+  format = function(entry, vim_item)
+    if entry.source.name == "cmdline" then
+      vim_item.kind = "⌘"
+      vim_item.menu = ""
+      return vim_item
+    end
+    vim_item.menu = cmp_sources[entry.source.name] or vim_item.kind
+    vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+    return vim_item
+  end,
+}
+cmp.setup.cmdline(":", {
+  mapping = mapping,
+  sources = {
+    { name = "path" },
+    { name = "cmdline", keyword_length = 2, keyword_pattern = [=[[^[:blank:]\!]*]=] },
+  },
+})
+
+cmp.setup.cmdline("/", {
+  preselect = cmp.PreselectMode.None,
+  mapping = mapping,
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype({ "gitcommit" }, {
+  mapping = mapping,
+  sources = cmp.config.sources({
+    {
+      name = "buffer",
+      option = {
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
+          end
+          return vim.tbl_keys(bufs)
+        end,
+      },
+    },
+  }),
+})
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,22 +1,23 @@
 -- Additional Plugins
 lvim.plugins = {
-	"mfussenegger/nvim-jdtls",
-	"karb94/neoscroll.nvim",
-	"j-hui/fidget.nvim",
-	"windwp/nvim-ts-autotag",
-	"kylechui/nvim-surround",
-	"christianchiarulli/harpoon",
-	"windwp/nvim-spectre",
-	"lvimuser/lsp-inlayhints.nvim",
-	"kdheepak/lazygit.nvim",
-	"junegunn/vim-easy-align",
-	{
-		"catppuccin/nvim",
-		as = "catppuccin",
-	},
-	{
-		"phaazon/hop.nvim",
-		branch = "v2",
-	},
-	"dapc11/vim-fugitive",
+  "mfussenegger/nvim-jdtls",
+  "karb94/neoscroll.nvim",
+  "j-hui/fidget.nvim",
+  "windwp/nvim-ts-autotag",
+  "kylechui/nvim-surround",
+  "christianchiarulli/harpoon",
+  "windwp/nvim-spectre",
+  "lvimuser/lsp-inlayhints.nvim",
+  "kdheepak/lazygit.nvim",
+  "junegunn/vim-easy-align",
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
+  },
+  {
+    "phaazon/hop.nvim",
+    branch = "v2",
+  },
+  "dapc11/vim-fugitive",
+  "hrsh7th/cmp-cmdline",
 }
