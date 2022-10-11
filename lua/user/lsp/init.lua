aa39604a14010283377cadbcb8f5969576a2d5b8
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-10-11 14:35:57 +0200

    Add gitsigns & trail_space code actions

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -43,6 +43,11 @@ linters.setup({
     },
   },
 })
+local null_ls = require("null-ls")
+lvim.lsp.null_ls.setup = {sources = {
+  null_ls.builtins.code_actions.gitsigns,
+  null_ls.builtins.diagnostics.trail_space,
+}}
 
 --- Golang
 local lsp_manager = require("lvim.lsp.manager")
