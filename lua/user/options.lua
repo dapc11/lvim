84198412e3f5aa0f5ba8c79d81970abf8b0d09d6
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-10-12 13:21:20 +0200

    disable nvimtree diagnostics

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -9,6 +9,7 @@ lvim.builtin.terminal.active = true
 lvim.builtin.dap.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.diagnostics.enable = false
 lvim.builtin.breadcrumbs.active = true
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.cmp.completion = { completion = { completeopt = "menu,menuone, noinsert" } }
