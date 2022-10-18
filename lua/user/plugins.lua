f15529a26e72c8b9958b7e3a59446e1cf4fcdac7
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-10-18 19:14:59 +0200

    Add colorizer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,4 +54,22 @@ lvim.plugins = {
       })
     end,
   },
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        user_default_options = {
+          RGB = true, -- #RGB hex codes
+          RRGGBB = true, -- #RRGGBB hex codes
+          names = false, -- "Name" codes like Blue or blue
+          RRGGBBAA = true, -- #RRGGBBAA hex codes
+          AARRGGBB = true, -- 0xAARRGGBB hex codes
+          rgb_fn = true, -- CSS rgb() and rgba() functions
+          -- Available modes for `mode`: foreground, background,  virtualtext
+          mode = "virtualtext", -- Set the display mode.
+          virtualtext = "■■■■■",
+        },
+      })
+    end,
+  },
 }
