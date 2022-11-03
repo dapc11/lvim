70b659c624810bb39855516356cc2cfff98a7e12
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-11-03 20:16:03 +0100

    Add Telescope multi_selection

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -80,24 +80,135 @@ lvim.builtin.telescope.defaults.path_display = { "truncate" }
 lvim.builtin.telescope.file_sorter = require("telescope.sorters").get_fuzzy_file
 lvim.builtin.telescope.generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter
 
+local actions_layout = require("telescope.actions.layout")
+local action_state = require("telescope.actions.state")
+local transform_mod = require("telescope.actions.mt").transform_mod
+
+local function multiopen(prompt_bufnr, method)
+    local edit_file_cmd_map = {
+        vertical = "vsplit",
+        horizontal = "split",
+        tab = "tabedit",
+        default = "edit",
+    }
+    local edit_buf_cmd_map = {
+        vertical = "vert sbuffer",
+        horizontal = "sbuffer",
+        tab = "tab sbuffer",
+        default = "buffer",
+    }
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local multi_selection = picker:get_multi_selection()
+
+    if #multi_selection > 1 then
+        require("telescope.pickers").on_close_prompt(prompt_bufnr)
+        pcall(vim.api.nvim_set_current_win, picker.original_win_id)
+
+        for i, entry in ipairs(multi_selection) do
+            local filename, row, col
+
+            if entry.path or entry.filename then
+                filename = entry.path or entry.filename
+
+                row = entry.row or entry.lnum
+                col = vim.F.if_nil(entry.col, 1)
+            elseif not entry.bufnr then
+                local value = entry.value
+                if not value then
+                    return
+                end
+
+                if type(value) == "table" then
+                    value = entry.display
+                end
+
+                local sections = vim.split(value, ":")
+
+                filename = sections[1]
+                row = tonumber(sections[2])
+                col = tonumber(sections[3])
+            end
+
+            local entry_bufnr = entry.bufnr
+
+            if entry_bufnr then
+                if not vim.api.nvim_buf_get_option(entry_bufnr, "buflisted") then
+                    vim.api.nvim_buf_set_option(entry_bufnr, "buflisted", true)
+                end
+                local command = i == 1 and "buffer" or edit_buf_cmd_map[method]
+                pcall(vim.cmd, string.format("%s %s", command, vim.api.nvim_buf_get_name(entry_bufnr)))
+            else
+                local command = i == 1 and "edit" or edit_file_cmd_map[method]
+                if vim.api.nvim_buf_get_name(0) ~= filename or command ~= "edit" then
+                    filename = require("plenary.path"):new(vim.fn.fnameescape(filename)):normalize(vim.loop.cwd())
+                    pcall(vim.cmd, string.format("%s %s", command, filename))
+                end
+            end
+
+            if row and col then
+                pcall(vim.api.nvim_win_set_cursor, 0, { row, col })
+            end
+        end
+    else
+        actions["select_" .. method](prompt_bufnr)
+    end
+end
+
+local custom_actions = transform_mod({
+    multi_selection_open_vertical = function(prompt_bufnr)
+        multiopen(prompt_bufnr, "vertical")
+    end,
+    multi_selection_open_horizontal = function(prompt_bufnr)
+        multiopen(prompt_bufnr, "horizontal")
+    end,
+    multi_selection_open_tab = function(prompt_bufnr)
+        multiopen(prompt_bufnr, "tab")
+    end,
+    multi_selection_open = function(prompt_bufnr)
+        multiopen(prompt_bufnr, "default")
+    end,
+})
+
+local function stopinsert(callback)
+    return function(prompt_bufnr)
+        vim.cmd.stopinsert()
+        vim.schedule(function()
+            callback(prompt_bufnr)
+        end)
+    end
+end
+
 lvim.builtin.telescope.defaults.mappings.i = {
   ["<C-p>"] = action_layout.toggle_preview,
   ["<C-c>"] = actions.close,
   ["<esc>"] = actions.close,
   ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-  ["<s-tab>"] = actions.toggle_selection + actions.move_selection_next,
-  ["<tab>"] = actions.toggle_selection + actions.move_selection_previous,
-  ["<CR>"] = actions.select_default,
-  ["<C-h>"] = actions.select_horizontal,
-  ["<C-v>"] = actions.select_vertical,
-  ["<C-o>"] = actions.select_tab,
+  ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+  ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+  ["<C-Down>"] = actions.cycle_history_next,
+  ["<C-Up>"] = actions.cycle_history_prev,
+  ["<M-right>"] = actions_layout.cycle_layout_next,
+  ["<M-left>"] = actions_layout.cycle_layout_prev,
+  ["<C-v>"] = stopinsert(custom_actions.multi_selection_open_vertical),
+  ["<C-h>"] = stopinsert(custom_actions.multi_selection_open_horizontal),
+  ["<C-t>"] = stopinsert(custom_actions.multi_selection_open_tab),
+  ["<CR>"] = stopinsert(custom_actions.multi_selection_open),
 }
 lvim.builtin.telescope.defaults.mappings.n = {
   ["<C-p>"] = action_layout.toggle_preview,
   ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-  ["<s-tab>"] = actions.toggle_selection + actions.move_selection_next,
-  ["<tab>"] = actions.toggle_selection + actions.move_selection_previous,
+  ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+  ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
   ["<C-c>"] = actions.close,
+  ["<C-Down>"] = actions.cycle_history_next,
+  ["<C-Up>"] = actions.cycle_history_prev,
+  ["<M-right>"] = actions_layout.cycle_layout_next,
+  ["<M-left>"] = actions_layout.cycle_layout_prev,
+  ["<C-o>"] = actions_layout.toggle_preview,
+  ["<C-v>"] = custom_actions.multi_selection_open_vertical,
+  ["<C-s>"] = custom_actions.multi_selection_open_horizontal,
+  ["<C-t>"] = custom_actions.multi_selection_open_tab,
+  ["<CR>"] = custom_actions.multi_selection_open,
 }
 
 lvim.builtin.telescope.file_previewer = {
