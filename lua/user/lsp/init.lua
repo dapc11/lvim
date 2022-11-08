141a8195f7c2be44be39f9c8678e59fa7c04d98a
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-11-08 19:53:07 +0100

    Remove duplicate golang setup

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -55,108 +55,6 @@ if not status_ok then
   print("Couldn't import which-key")
   return
 end
---- Golang
-local lsp_manager = require("lvim.lsp.manager")
-lsp_manager.setup("golangci_lint_ls", {
-  on_init = require("lvim.lsp").common_on_init,
-  capabilities = require("lvim.lsp").common_capabilities(),
-})
-
-local go_opts = {
-  settings = {
-    gopls = {
-      gofumpt = false, -- A stricter gofmt
-      codelenses = {
-        gc_details = true, -- Toggle the calculation of gc annotations
-        generate = true, -- Runs go generate for a given directory
-        regenerate_cgo = true, -- Regenerates cgo definitions
-        test = true,
-        tidy = true, -- Runs go mod tidy for a module
-        upgrade_dependency = true, -- Upgrades a dependency in the go.mod file for a module
-        vendor = true, -- Runs go mod vendor for a module
-      },
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-      diagnosticsDelay = "300ms",
-      experimentalWatchedFileDelay = "100ms",
-      symbolMatcher = "fuzzy",
-      completeUnimported = true,
-      staticcheck = true,
-      matcher = "Fuzzy",
-      usePlaceholders = true, -- enables placeholders for function parameters or struct fields in completion responses
-      analyses = {
-        fieldalignment = true, -- find structs that would use less memory if their fields were sorted
-        nilness = true, -- check for redundant or impossible nil comparisons
-        shadow = true, -- check for possible unintended shadowing of variables
-        unusedparams = true, -- check for unused parameters of functions
-        unusedwrite = true, -- checks for unused writes, an instances of writes to struct fields and arrays that are never read
-      },
-    },
-  },
-  on_attach = function(client, bufnr)
-    require("lvim.lsp").common_on_attach(client, bufnr)
-    local _, _ = pcall(vim.lsp.codelens.refresh)
-    local opts = {
-      mode = "n", -- NORMAL mode
-      prefix = "<leader>",
-      buffer = bufnr, -- Global mappings. Specify a buffer number for buffer local mappings
-      silent = true, -- use `silent` when creating keymaps
-      noremap = true, -- use `noremap` when creating keymaps
-      nowait = true, -- use `nowait` when creating keymaps
-    }
-    local mappings = {
-      D = {
-        name = "Go Debug",
-        t = { "<cmd>lua require'dap-go'.debug_test()<cr>", "Debug test under cursor" },
-        T = { "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test" },
-      },
-      L = {
-        name = "Go",
-        i = { "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies" },
-        t = { "<cmd>GoMod tidy<cr>", "Tidy" },
-        a = { "<cmd>GoTestAdd<Cr>", "Add Test" },
-        A = { "<cmd>GoTestsAll<Cr>", "Add All Tests" },
-        e = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
-        g = { "<cmd>GoGenerate<Cr>", "Go Generate" },
-        f = { "<cmd>GoGenerate %<Cr>", "Go Generate File" },
-        c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
-        r = { "<cmd>GoIfErr<Cr>", "Add if err" },
-      },
-    }
-    which_key.register(mappings, opts)
-  end,
-}
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = { "*.go" },
-  callback = function()
-    local _, _ = pcall(vim.lsp.codelens.refresh)
-  end,
-})
-
-lsp_manager.setup("gopls", go_opts)
-
-local status_ok, gopher = pcall(require, "gopher")
-if not status_ok then
-  return
-end
-
-gopher.setup({
-  commands = {
-    go = "go",
-    gomodifytags = "gomodifytags",
-    gotests = "gotests",
-    impl = "impl",
-    iferr = "iferr",
-  },
-})
---- /Golang
 
 --- Python
 local python_opts = {
@@ -217,6 +115,7 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   end,
 })
 
+local lsp_manager = require("lvim.lsp.manager")
 lsp_manager.setup("pyright", python_opts)
 
 local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
