ed83b51c6a2c1613c0a9ed7f64da6904f24b2c54
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-11-09 09:15:10 +0100

    Setup treesitter textobjects

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -21,3 +21,66 @@ parser_config.gotmpl = {
   filetype = "gotmpl",
   used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml", "tpl", "yml" },
 }
+
+lvim.builtin.treesitter.textobjects.enable = true
+lvim.builtin.treesitter.textobjects.keymaps = {
+  ["iL"] = {
+    -- you can define your own textobjects directly here
+    go = "(function_definition) @function",
+  },
+  -- or you use the queries from supported languages with textobjects.scm
+  ["af"] = "@function.outer",
+  ["if"] = "@function.inner",
+  ["aC"] = "@class.outer",
+  ["iC"] = "@class.inner",
+  ["ac"] = "@conditional.outer",
+  ["ic"] = "@conditional.inner",
+  ["ae"] = "@block.outer",
+  ["ie"] = "@block.inner",
+  ["al"] = "@loop.outer",
+  ["il"] = "@loop.inner",
+  ["is"] = "@statement.inner",
+  ["as"] = "@statement.outer",
+  ["ad"] = "@comment.outer",
+  ["am"] = "@call.outer",
+  ["im"] = "@call.inner",
+}
+
+lvim.builtin.treesitter.textobjects.move = {
+  enable = true,
+  set_jumps = true, -- whether to set jumps in the jumplist
+  goto_next_start = {
+    ["<m"] = "@function.outer",
+    ["<<"] = "@class.outer",
+  },
+  goto_next_end = {
+    ["<M"] = "@function.outer",
+    ["<>"] = "@class.outer",
+  },
+  goto_previous_start = {
+    [">m"] = "@function.outer",
+    [">>"] = "@class.outer",
+  },
+  goto_previous_end = {
+    [">M"] = "@function.outer",
+    ["><"] = "@class.outer",
+  },
+}
+lvim.builtin.treesitter.textobjects.select = {
+  enable = true,
+  keymaps = {
+    -- You can use the capture groups defined in textobjects.scm
+    ["af"] = "@function.outer",
+    ["if"] = "@function.inner",
+    ["ac"] = "@class.outer",
+    ["ic"] = "@class.inner",
+    -- Or you can define your own textobjects like this
+    ["iF"] = {
+      python = "(function_definition) @function",
+      cpp = "(function_definition) @function",
+      c = "(function_definition) @function",
+      java = "(method_declaration) @function",
+      go = "(method_declaration) @function",
+    },
+  },
+}
