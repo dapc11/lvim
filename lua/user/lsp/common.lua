ffd44c2134d4b91f5c5a3d283be813d4a16c1f2e
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-11-16 21:14:57 +0100

    Setup sumneko_lua

--- lua/user/lsp/common.lua
+++ lua/user/lsp/common.lua
@@ -1,17 +1,2 @@
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls", "pyright", "gopls", "yamlls" })
 lvim.lsp.diagnostics.virtual_text = false
--- lvim.lsp.diagnostics.virtual_text = {
---   source = "always",
---   severity = vim.diagnostic.severity.ERROR,
---   spacing = 1,
--- }
-
-lvim.lsp.installer.setup.ensure_installed = {
-  "sumneko_lua",
-  "jsonls",
-}
-
--- lvim.format_on_save = {
---   pattern = { "*.lua", "*.py", "*.go" },
--- }
 lvim.format_on_save = false
--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -4,6 +4,7 @@ lvim.lsp.diagnostics.virtual_text = {
   spacing = 1,
 }
 
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls", "pyright", "gopls", "yamlls" })
 lvim.lsp.installer.setup.ensure_installed = {
   "sumneko_lua",
   "jsonls",
