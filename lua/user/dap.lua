a01c00e606b447f05b6bb2eb6c1532d995effdf8
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-11-26 21:21:25 +0100

    Use virtualenv python executable

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -1,18 +1,14 @@
 local dap = require("dap")
 dap.adapters.python = {
   type = "executable",
-  command = "python3",
+  command = os.getenv("VIRTUAL_ENV") .. "/bin/python",
   args = { "-m", "debugpy.adapter" },
 }
 dap.configurations.python = {
   {
-    -- The first three options are required by nvim-dap
     type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
     request = "launch",
     name = "Launch file",
-
-    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
     program = "${file}", -- This configuration will launch the current file if used.
     pythonPath = "python3",
   },
