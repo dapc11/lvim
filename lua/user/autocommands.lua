50e743f0bbd43ce9d4f96e116f0559428e12acff
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-12-02 22:39:35 +0100

    Implement support for pythonpath per project

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,165 +1,220 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = {
-		"Jaq",
-		"qf",
-		"help",
-		"man",
-		"lspinfo",
-		"spectre_panel",
-		"lir",
-		"DressingSelect",
-		"tsplayground",
-		"Markdown",
-	},
-	callback = function()
-		vim.cmd([[
+  pattern = {
+    "Jaq",
+    "qf",
+    "help",
+    "man",
+    "lspinfo",
+    "spectre_panel",
+    "lir",
+    "DressingSelect",
+    "tsplayground",
+    "Markdown",
+  },
+  callback = function()
+    vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR>
       nnoremap <silent> <buffer> <esc> :close<CR>
       set nobuflisted
     ]])
-	end,
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "" },
-	callback = function()
-		local buf_ft = vim.bo.filetype
-		if buf_ft == "" or buf_ft == nil then
-			vim.cmd([[
+  pattern = { "" },
+  callback = function()
+    local buf_ft = vim.bo.filetype
+    if buf_ft == "" or buf_ft == nil then
+      vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR>
       nnoremap <silent> <buffer> <c-j> j<CR>
       nnoremap <silent> <buffer> <c-k> k<CR>
       set nobuflisted
     ]])
-		end
-	end,
+    end
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "" },
-	callback = function()
-		local get_project_dir = function()
-			local cwd = vim.fn.getcwd()
-			local project_dir = vim.split(cwd, "/")
-			local project_name = project_dir[#project_dir]
-			return project_name
-		end
+  pattern = { "" },
+  callback = function()
+    local get_project_dir = function()
+      local cwd = vim.fn.getcwd()
+      local project_dir = vim.split(cwd, "/")
+      local project_name = project_dir[#project_dir]
+      return project_name
+    end
 
-		vim.opt.titlestring = get_project_dir() .. " - nvim"
+    vim.opt.titlestring = get_project_dir() .. " - nvim"
     vim.opt.laststatus = 3
-	end,
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "term://*" },
-	callback = function()
-		vim.cmd("startinsert!")
-		vim.cmd("set cmdheight=1")
-	end,
+  pattern = { "term://*" },
+  callback = function()
+    vim.cmd("startinsert!")
+    vim.cmd("set cmdheight=1")
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "gitcommit", "markdown" },
-	callback = function()
-		vim.opt_local.wrap = true
-		vim.opt_local.spell = true
-	end,
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-	callback = function()
-		vim.cmd("tabdo wincmd =")
-	end,
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
-	callback = function()
-		vim.cmd("quit")
-	end,
+  callback = function()
+    vim.cmd("quit")
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-	callback = function()
-		vim.cmd("set formatoptions-=cro")
-	end,
+  callback = function()
+    vim.cmd("set formatoptions-=cro")
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-	callback = function()
-		vim.highlight.on_yank({ higroup = "Visual", timeout = 40 })
-	end,
+  callback = function()
+    vim.highlight.on_yank({ higroup = "Visual", timeout = 40 })
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
-	callback = function()
-		vim.cmd("hi link illuminatedWord LspReferenceText")
-	end,
+  callback = function()
+    vim.cmd("hi link illuminatedWord LspReferenceText")
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-	pattern = { "*" },
-	callback = function()
-		vim.cmd("checktime")
-	end,
+  pattern = { "*" },
+  callback = function()
+    vim.cmd("checktime")
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "CursorHold" }, {
-	callback = function()
-		local status_ok, luasnip = pcall(require, "luasnip")
-		if not status_ok then
-			return
-		end
-		if luasnip.expand_or_jumpable() then
-			-- ask maintainer for option to make this silent
-			-- luasnip.unlink_current()
-			vim.cmd([[silent! lua require("luasnip").unlink_current()]])
-		end
-	end,
+  callback = function()
+    local status_ok, luasnip = pcall(require, "luasnip")
+    if not status_ok then
+      return
+    end
+    if luasnip.expand_or_jumpable() then
+      -- ask maintainer for option to make this silent
+      -- luasnip.unlink_current()
+      vim.cmd([[silent! lua require("luasnip").unlink_current()]])
+    end
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = "*",
-    command = [[
+  pattern = "*",
+  command = [[
       let current_pos = getpos(".")
       silent! %s/\v\s+$|\n+%$//e
       silent! call setpos(".", current_pos)
 		]],
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "py" },
-    callback = function()
-      vim.opt_local.shiftwidth = 4
-      vim.opt_local.tabstop = 4
-      vim.opt_local.softtabstop = 4
-      vim.opt_local.expandtab = true
-    end,
+  pattern = { "python" },
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+local function match(dir, pattern)
+  if string.sub(pattern, 1, 1) == "=" then
+    return vim.fn.fnamemodify(dir, ":t") == string.sub(pattern, 2, #pattern)
+  else
+    return vim.fn.globpath(dir, pattern) ~= ""
+  end
+end
+
+local function parent_dir(dir)
+  return vim.fn.fnamemodify(dir, ":h")
+end
+
+local function get_root(pythonpath_file)
+  local current = vim.api.nvim_buf_get_name(0)
+  local parent = parent_dir(current)
+
+  while 1 do
+    if match(parent, pythonpath_file) then
+      return parent
+    end
+
+    current, parent = parent, parent_dir(parent)
+    if parent == current then
+      break
+    end
+  end
+  return nil
+end
+
+local function file_exists(name)
+  local f = io.open(name, "r")
+  return f ~= nil and io.close(f)
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = "*",
+  callback = function()
+    local buf_ft = vim.bo.filetype
+    if buf_ft ~= "python" or buf_ft == nil then
+      return
+    end
+    local pythonpath_file = ".pythonpath"
+    local root = get_root(pythonpath_file)
+    if root == nil then
+      vim.env.PYTHONPATH = ""
+      return
+    end
+    local absolute_path = root .. "/" .. pythonpath_file
+    if file_exists(absolute_path) then
+      for line in io.open(absolute_path):lines() do
+        vim.env.PYTHONPATH = line
+      end
+    end
+  end,
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "yml, yaml" },
-    callback = function()
-      vim.opt_local.shiftwidth = 2
-      vim.opt_local.tabstop = 2
-      vim.opt_local.softtabstop = 2
-      vim.opt_local.expandtab = true
-      vim.opt_local.indentkeys:append("0#")
-      vim.opt_local.indentkeys:append("<:>")
-    end,
+  pattern = { "yml, yaml" },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+    vim.opt_local.indentkeys:append("0#")
+    vim.opt_local.indentkeys:append("<:>")
+  end,
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "lua" },
-    callback = function()
-      vim.opt_local.shiftwidth = 2
-      vim.opt_local.tabstop = 2
-      vim.opt_local.softtabstop = 2
-      vim.opt_local.expandtab = true
-    end,
+  pattern = { "lua" },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "xml" },
-    callback = function()
-      vim.opt_local.shiftwidth = 4
-      vim.opt_local.tabstop = 4
-      vim.opt_local.softtabstop = 4
-      vim.opt_local.expandtab = true
-    end,
+  pattern = { "xml" },
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+  end,
 })
