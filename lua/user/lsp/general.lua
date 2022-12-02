1f310442530dc4f01c23f0b6a1fbd7ec8c9f53fc
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-12-02 19:39:26 +0100

    Lsp General

--- lua/user/lsp/general.lua
+++ lua/user/lsp/general.lua
@@ -1,9 +1,9 @@
+
 lvim.lsp.diagnostics.virtual_text = {
   source = "always",
   severity = vim.diagnostic.severity.ERROR,
   spacing = 1,
 }
-
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls", "gopls", "yamlls" })
 lvim.lsp.installer.setup.ensure_installed = {
   "pyright",
