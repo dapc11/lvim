d5efaa2f69dec66679fdad3bda893e3812f11552
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-12-16 08:44:27 +0100

    Remap pick_venv

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -4,8 +4,7 @@ map("n", "gf", "<cmd>PytrizeJumpFixture<Cr>", "Jump To Fixture", bufnr)
 map("n", "<leader>Dm", "<cmd>lua require('dap-python').test_method()<cr>", "Test Method", bufnr)
 map("n", "<leader>Df", "<cmd>lua require('dap-python').test_class()<cr>", "Test Class", bufnr)
 map("n", "<leader>Ds", "<cmd>lua require('dap-python').debug_selection()<cr>", "Debug Selection", bufnr)
-map("n", "<leader>Ci", "<cmd>lua require('swenv.api').pick_venv()<cr>", "Pick Env", bufnr)
-map("n", "<leader>Cd", "<cmd>lua require('swenv.api').get_current_venv()<cr>", "Show Env", bufnr)
+map("n", "<leader>Ca", "<cmd>lua require('swenv.api').pick_venv()<cr>", "Pick Env", bufnr)
 
 pcall(function()
   require("dap-python").test_runner = "pytest"
