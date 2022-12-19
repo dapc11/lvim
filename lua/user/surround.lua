055ca36401cb117edacf0809e4ac1f79759d3994
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-12-19 07:59:40 +0100

    Remove clashing leap delete keymap

--- lua/user/surround.lua
+++ lua/user/surround.lua
@@ -3,4 +3,17 @@ if not status_ok then
   return
 end
 
-surround.setup()
+surround.setup({
+  keymaps = {
+    insert = "<C-g>s",
+    insert_line = "<C-g>S",
+    normal = "ys",
+    normal_cur = "yss",
+    normal_line = "yS",
+    normal_cur_line = "ySS",
+    visual = "S",
+    visual_line = "gS",
+    delete = "sd",
+    change = "cs",
+  },
+})
