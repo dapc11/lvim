a733e695578973476d3ba1864427328ba56c0469
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-12-20 13:04:08 +0100

    Formatting

--- lua/user/harpoon.lua
+++ lua/user/harpoon.lua
@@ -1,10 +1,10 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-	return
+  return
 end
 
 local h_status_ok, _ = pcall(require, "harpoon")
 if not h_status_ok then
-	return
+  return
 end
 telescope.load_extension("harpoon")
