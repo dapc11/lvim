1caafac8c7d6dbb996d7134c5738de9e492d1f48
Daniel Tingelöf <daniel.tingelof@gmail.com>
2023-01-20 19:28:56 +0100

    Add incremental_selection bound to <CR> in normal mode

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -87,3 +87,15 @@ lvim.builtin.treesitter.textobjects.select = {
     },
   },
 }
+
+require("nvim-treesitter.configs").setup({
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      node_incremental = "<CR>",
+      scope_incremental = "<S-CR>",
+      node_decremental = "<BS>",
+    },
+  },
+})
