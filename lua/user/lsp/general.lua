52de433f750325112bb15ab04057837aa772c048
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-02-24 08:01:34 +0100

    Replace sumneko with lua_ls

--- lua/user/lsp/general.lua
+++ lua/user/lsp/general.lua
@@ -2,6 +2,6 @@ lvim.lsp.diagnostics.virtual_text = false
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls", "gopls", "yamlls" })
 lvim.lsp.installer.setup.ensure_installed = {
   "pyright",
-  "sumneko_lua",
+  "lua_ls",
   "jsonls",
 }
