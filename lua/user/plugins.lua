75eb3c7088a7e4c343a4cc284463ab42f10aade1
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-03-08 07:01:45 +0100

    Tmux navigation

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -145,10 +145,10 @@ lvim.plugins = {
       require("nvim-tmux-navigation").setup({
         disable_when_zoomed = true, -- defaults to false
         keybindings = {
-          left = "<C-h>",
-          down = "<C-j>",
-          up = "<C-k>",
-          right = "<C-l>",
+          left = "<C-left>",
+          down = "<C-down>",
+          up = "<C-up>",
+          right = "<C-right>",
           last_active = "<C-\\>",
           next = "<C-Space>",
         },
