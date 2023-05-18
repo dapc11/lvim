b66666ff1c7a4463e46b13d46d36b81e9dd6ab6e
Daniel Tingelöf <daniel.tingelof@gmail.com>
2023-05-18 21:31:42 +0200

    Update gitcommit and markdown

--- ftplugin/gitcommit.lua
+++ ftplugin/gitcommit.lua
@@ -1,5 +1,6 @@
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
+vim.opt.wrap = true
 
 local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "<s", "]s", { buffer = bufnr })
--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,7 +2,7 @@ local bufnr = vim.api.nvim_get_current_buf()
 require("which-key").register({
   ["K"] = { vim.lsp.buf.hover, "Preview a Linked Note", buffer = bufnr },
   ["gd"] = { vim.lsp.buf.definition, "Follow Link", buffer = bufnr },
-  ["<leader>zL"] = { "<Cmd>ZkBacklinks<CR>", "Open Notes Linking to Buffer", buffer = bufnr },
+  ["<leader>zL"] = { vim.cmd.ZkBacklinks, "Open Notes Linking to Buffer", buffer = bufnr },
   ["<leader>zf"] = { vim.lsp.buf.definition, "Follow Link", buffer = bufnr },
   ["<leader>za"] = { ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", "Code Actions", buffer = bufnr, mode = "v" },
   ["<leader>zl"] = { vim.cmd.ZkLinks, "Open Notes Linked by the Buffer", buffer = bufnr, mode = { "v", "n" } },
@@ -10,7 +10,7 @@ require("which-key").register({
 
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
+vim.opt.wrap = true
 
-local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "<s", "]s", { buffer = bufnr })
 vim.keymap.set("n", ">s", "[s", { buffer = bufnr })
